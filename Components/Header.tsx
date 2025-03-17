9a7730ba27118e52c8c2494b725a887eb95672f8
Tomas Gonzalez <tpg3@njit.edu>
2025-03-17 00:00:05 -0400

    projects section done

---
+++ Components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function Navbar(){
+    return(
+        <header className='from-gray-900 to-black text-white w-full p-4 border-2 border-gray-900 '>
+        <nav>
+        <ul className='flex space-x-6 justify-center'>
+            <li><Link href="/">
+                Projects
+            </Link>
+            </li>
+            <li><Link href="/about">
+                About Me
+            </Link>
+            </li>
+            <li><Link href="/resume">
+                Resume
+            </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
---
+++ pages/resume.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "../Components/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Resume() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <Navbar/>
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+            <h1> Resume</h1>
+        </main>
+        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+
+
+
+        </footer>
+    </div>
+  );
+}
